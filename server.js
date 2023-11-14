ac3af7b2e7b6cf9204803e0f39d05cdf7c111947
Aniket Jain <aniketjain2018@gmail.com>
2023-11-14 19:37:25 +0530

    done

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const endpointSecret =
 const express = require("express");
 require('dotenv').config()
 const app = express();
-// app.use(express.json());
+app.use(express.json());
 
 
 app.get("/" , (req, res) => {
